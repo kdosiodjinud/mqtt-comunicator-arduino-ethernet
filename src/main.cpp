bbd106d275bd9acb60737854ee4d4e4758075a1e
Jan Kuthan <jan.kuthan@lmc.eu>
2020-03-24 15:12:14 +0100

    Use pullUp resistors for check actual sensor value. Reconnect after ethernet failure.

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 
  //############ CONFIGURATION ################
  // wifi
-const char* ssid = "Bifrost";
-const char* password =  "CbSsPp42";
+// const char* ssid = "NazevSite";
+// const char* password =  "heslo";
+
 // ethernet 
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 199);
+const char* hostname = "RelayBoard1";
+
  // mqtt server
 const char* mqttServer = "192.168.1.100";
 const int mqttPort = 1883;
@@ -25,32 +27,54 @@ struct settingsOutput {
   bool defaultLogic;
   bool pingAndReturn;
   bool valueForPing;
-} settingsOutput[1] = {
-  { "dum/pracovna/svetla/hlavni", DD3, HIGH, true, LOW }
-  // { "dum/technicka/svetla/hlavni", DD1, HIGH, LOW },
-  // { "dum/predsin/svetla/hlavni", DD2, HIGH, LOW },
-  // { "dum/dolnichodba/svetla/hlavni", DD3, HIGH, LOW },
-  // { "dum/spiz/svetla/hlavni", DD4, HIGH, LOW },
-  // { "dum/dolnikoupelna/svetla/hlavni", DD5, HIGH, LOW },
-  // { "dum/kuchyn/svetla/hlavni", DD6, HIGH, LOW },
-  // { "dum/jidelna/svetla/hlavni", DD7, HIGH, LOW },
-  // { "dum/obyvak/svetla/hlavni", DDA0, HIGH, LOW }
+} settingsOutput[16] = {
+  // SVETLA
+  { "dum/pracovna/svetla/hlavni", 22, HIGH, true, LOW }, // PRACOVNA
+  { "dum/technicka/svetla/hlavni", 23, HIGH, true, LOW }, // TECHNICKA
+  { "dum/predsin/svetla/hlavni", 24, HIGH, true, LOW }, // PREDSIN
+  { "dum/dolnichodba/svetla/hlavni", 25, HIGH, true, LOW }, // DOLNI CHODBA
+  { "dum/spiz/svetla/hlavni", 26, HIGH, true, LOW }, // SPIZ
+  { "dum/dolnikoupelna/svetla/hlavni", 27, HIGH, true, LOW }, // DOLNI KOUPELNA
+  { "dum/kuchyn/svetla/hlavni", 28, HIGH, true, LOW }, // KUCHYN
+  { "dum/jidelna/svetla/hlavni", 29, HIGH, true, LOW }, // JIDELNA
+  { "dum/obyvak/svetla/hlavni", 30, HIGH, true, LOW }, // OBYVAK
+  { "dum/schody/svetla/hlavni", 31, HIGH, true, LOW }, // SCHODY
+  { "dum/hornichodba/svetla/hlavni", 32, HIGH, true, LOW }, // HORNI CHODBA
+  { "dum/hornikoupelna/svetla/hlavni", 33, HIGH, true, LOW }, // HORNI KOUPELNA
+  { "dum/koupelna/svetla/hlavni", 34, HIGH, true, LOW }, // ZRDCADLA
+  { "dum/loznice/svetla/hlavni", 35, HIGH, true, LOW }, // LOŽNICE
+
+  // ZASUVKY
+  { "dum/kuchyn/zasuvky/drezLeva", 36, HIGH, true, LOW }, // ZASUVKY KUCHYN 1
+  { "dum/hornichodba/zasuvky/uprostred", 37, HIGH, true, LOW } // ZASUVKA HORNI CHODBA
 };
 
 struct settingsInput {
   char* publishToTopic;
   int pin;
-} settingsInput[1] = {
-  { "dum/pracovna/svetla/hlavniPotvrzeni", DD4 }
-  // { "dum/technicka/svetla/hlavniPotvrzeni", DDA2 },
-  // { "dum/predsin/svetla/hlavniPotvrzeni", DDA3 },
-  // { "dum/dolnichodba/svetla/hlavniPotvrzeni", DDA4 },
-  // { "dum/spiz/svetla/hlavniPotvrzeni", DDA5 },
-  // { "dum/dolnikoupelna/svetla/hlavniPotvrzeni", DDA6 },
-  // { "dum/kuchyn/svetla/hlavniPotvrzeni", DDA7 },
-  // { "dum/jidelna/svetla/hlavniPotvrzeni", DDA },
-  // { "dum/obyvak/svetla/hlavniPotvrzeni", D8 }
+  bool publishTrueIfInputLow;
+} settingsInput[0] = {
+  // SVETLA
+  // { "dum/pracovna/svetla/hlavniPotvrzeni", 38, true}, // PRACOVNA
+  // { "dum/technicka/svetla/hlavniPotvrzeni", 39, true}, // TECHNICKA
+  // { "dum/predsin/svetla/hlavniPotvrzeni", 40, true}, // PREDSIN
+  // { "dum/dolnichodba/svetla/hlavniPotvrzeni", 41, true}, // DOLNI CHODBA
+  // { "dum/spiz/svetla/hlavniPotvrzeni", 42, true}, // SPIZ
+  // { "dum/dolnikoupelna/svetla/hlavniPotvrzeni", 43, true}, // DOLNI KOUPELNA
+  // { "dum/kuchyn/svetla/hlavniPotvrzeni", 44, true}, // KUCHYN
+  // { "dum/jidelna/svetla/hlavniPotvrzeni", 45, true}, // JIDELNA
+  // { "dum/obyvak/svetla/hlavniPotvrzeni", 46, true}, // OBYVAK
+  // { "dum/schody/svetla/hlavniPotvrzeni", 47, true}, // SCHODY
+  // { "dum/hornichodba/svetla/hlavniPotvrzeni", 48, true}, // HORNI CHODBA
+  // { "dum/hornikoupelna/svetla/hlavniPotvrzeni", 49, true}, // HORNI KOUPELNA
+  // { "dum/koupelna/svetla/hlavniPotvrzeni", 50, true}, // ZRDCADLA
+  // { "dum/loznice/svetla/hlavniPotvrzeni", 51, true}, // LOŽNICE
+
+  // ZASUVKY
+  // { "dum/kuchyn/zasuvky/drezLevaPotvrzeni", 52, true}, // ZASUVKY KUCHYN 1
+  // { "dum/hornichodba/zasuvky/uprostredPotvrzeni", 53, true} // ZASUVKA HORNI CHODBA
 };
+
 
 int settingsOutputInitial[99];
 int settingsInputPrevious[99];
@@ -60,6 +84,7 @@ int settingsInputPrevious[99];
 EthernetClient ethClient;
 PubSubClient client(ethClient);
 AsyncDelay delay_1s;
+AsyncDelay delay_ethCheckConnection;
 
 // main methods definitions
 void connectWifi();
@@ -68,12 +93,13 @@ void connectMqtt();
 void restartAllConnections();
 void callback(char* topic, byte* payload, unsigned int length);
 void setPortViaMqttTopicStatus(int port, char *topic, byte *payload, unsigned int length, bool pingAndReturn, bool valueForPing);
-void publishPortStatusToMqtt(char* topic, int port);
+void publishPortStatusToMqtt(char* topic, int port, bool publishTrueIfInputLow);
 
 // master setup method
 void setup() {
   delay(1000);
   delay_1s.start(1000, AsyncDelay::MILLIS);
+  delay_ethCheckConnection.start(5000, AsyncDelay::MILLIS);
   Serial.begin(115200);
 
   // prepare output pins
@@ -87,6 +113,10 @@ void setup() {
   Serial.println("Setting pin mode for inputs.");
   for (int i =0; i < sizeof(settingsInput) / sizeof(settingsInput[0]); i++) {
     pinMode(settingsInput[i].pin, INPUT);
+  
+    if (settingsInput[i].publishTrueIfInputLow) {
+      digitalWrite(settingsInput[i].pin, HIGH); // Enable pull-up rezistors
+    }
   }
 
   // connectWifi();
@@ -95,13 +125,27 @@ void setup() {
 }
 
 void loop() {
-  client.loop();
-    for (int i =0; i < sizeof(settingsInput) / sizeof(settingsInput[0]); i++) {
-      if (digitalRead(settingsInput[i].pin) != settingsInputPrevious[i]) {
-        publishPortStatusToMqtt(settingsInput[i].publishToTopic, settingsInput[i].pin);
-        settingsInputPrevious[i] = digitalRead(settingsInput[i].pin);
-      }
+
+  if (delay_ethCheckConnection.isExpired()) {
+    if (!ethClient.connected()) {
+        Serial.println("Connection lost... Reconnecting...");
+        connectEthernet();
+        connectMqtt();
     }
+
+    delay_ethCheckConnection.repeat();
+  }
+
+  client.loop();
+
+  //todo: načíst z MQTT previous status, než se začne publishovat (nebo to vyřeší hassio tím, že bude znát aktuální stav? - zkusit)
+
+  for (int i =0; i < sizeof(settingsInput) / sizeof(settingsInput[0]); i++) {
+    if (digitalRead(settingsInput[i].pin) != settingsInputPrevious[i]) {
+      publishPortStatusToMqtt(settingsInput[i].publishToTopic, settingsInput[i].pin, settingsInput[i].publishTrueIfInputLow);
+      settingsInputPrevious[i] = digitalRead(settingsInput[i].pin);
+    }
+  }
 }
 
 // MQTT subscribe
@@ -130,8 +174,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println("-----------------------");
 }
 
-void publishPortStatusToMqtt(char* topic, int pin) {
-  if (client.publish(topic, String(digitalRead(pin) ? "ON" : "OFF").c_str(), true)) {
+void publishPortStatusToMqtt(char* topic, int pin, bool publishTrueIfInputLow) {
+
+  bool pinValue = digitalRead(pin);
+
+  if (publishTrueIfInputLow) {
+    pinValue = pinValue ? false : true;
+  }
+
+  if (client.publish(topic, String(pinValue ? "ON" : "OFF").c_str(), true)) {
     Serial.print("Publish status ");
     Serial.print(digitalRead(pin));
     Serial.print(" for pin ");
@@ -189,13 +240,13 @@ void setPortViaMqttTopicStatus(int port, char *topic, byte *payload, unsigned in
 // }
 
 void connectEthernet() {
-  Ethernet.begin(mac, ip);
+  Ethernet.begin(mac);
 }
 
 void connectMqtt() {
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
- 
+
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
     if (!client.connect("deviceId", mqttUser, mqttPassword )) {
@@ -216,7 +267,7 @@ void restartAllConnections() {
       // WiFi.disconnect();
       // connectWifi();
 
-      Ethernet.begin(mac, ip);
+      Ethernet.begin(mac);
 
       Serial.println("Restart MQTT connection...");
       client.disconnect();
